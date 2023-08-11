8f28bea4ebd48d88ffcc954c004511111ebf4c44
rkafrawi <98607591+rkafrawi@users.noreply.github.com>
2023-08-11 10:47:26 -0400

    Add files via upload

---
+++ myServer.R
@@ -0,0 +1,353 @@
+# load vis source files:
+
+#define server
+function(input, output, session) {
+  
+  ### Define reactive values for global vars...next ver ###
+  # seuratData <- reactiveVal()
+  
+  ### input logic ###
+  
+  # Read Seurat object
+  seuratData <- reactive({
+    inFile <- input$seuratFile 
+    if (is.null(inFile))
+      return(NULL)
+    
+    #read Seurat object from selected file
+    seurat_obj <- readRDS(inFile$datapath)
+  
+    return(seurat_obj)
+  })
+  
+  # Render counts table (head)
+  output$table <- DT::renderDataTable({
+    #retrieve seurat object from reactive expression in above code block
+    seurat_data <- seuratData() 
+    if (is.null(seurat_data))
+      return(NULL)
+    # Metadata
+    seurat_df <- as.data.frame(seurat_data@meta.data)
+    datatable(seurat_df)
+  })
+  
+  ### featureplot logic ###
+  #umap featureplot logic
+  observeEvent(input$plotButton_UMAP, {
+    req(seuratData(),input$featuresInput) #wait for file input and feature input
+    seurat_data <- seuratData()
+    if (is.null(seurat_data))
+      output$errormessage <- renderText("Null file")
+    
+    # Split the input string @ commas into individual gene names using regex
+    #[[1]] extracts the first element in list of gene names, which gives us a chr vector containing the individaul gene names as separate elements
+    gene_names <- strsplit(input$featuresInput, ",\\s*")[[1]] 
+    gene_names <- trimws(gene_names)  # Trim leading and trailing spaces from gene names
+    
+    # Check if any of the requested genes are missing
+    missing_genes <- setdiff(gene_names, rownames(seurat_data@assays$RNA@data))
+    
+    if (length(missing_genes) > 0) {
+      # output$nofeaturefound <- renderText(paste("The following genes were not found:", paste(missing_genes, collapse = ", "))) #this worked... trying to phase out in favor of warning message
+      #warning message
+      shinyalert::shinyalert(
+        title = "Warning: Invalid Input",
+        text = paste("The following genes were not found:", paste(missing_genes, collapse = ", ")),
+        type = "warning")
+    } else {
+      # Generate feature plot
+      feature_plot <- FeaturePlot(object = seurat_data, features = gene_names,reduction = "umap")
+      
+      #assign featureplot to global env
+      assign("feature_plot", "new", envir = .GlobalEnv) #this doesnt seem to work... try declaring reactive vals at top of server script
+      
+      #make sure featureplot is not null before rendering plot
+      if (is.null(feature_plot)) {
+        output$nogenesfound <- renderText("No genes found!")
+      } else {
+        output$featurePlotUMAP <- renderPlot({ feature_plot })
+      }
+    }
+  })
+  
+  #pca featureplot logic
+  observeEvent(input$plotButton_PCA, {
+    req(seuratData(),input$featuresInput) #wait for file input and feature input
+    seurat_data <- seuratData()
+    if (is.null(seurat_data))
+      output$errormessage <- renderText("Null file")
+    
+    # Split the input string @ commas into individual gene names using regex
+    #[[1]] extracts the first element in list of gene names, which gives us a chr vector containing the individaul gene names as separate elements
+    gene_names <- strsplit(input$featuresInput, ",\\s*")[[1]] 
+    gene_names <- trimws(gene_names)  # Trim leading and trailing spaces from gene names
+    
+    # Check if any of the requested genes are missing
+    missing_genes <- setdiff(gene_names, rownames(seurat_data@assays$RNA@data))
+    
+    if (length(missing_genes) > 0) {
+      # output$nofeaturefound <- renderText(paste("The following genes were not found:", paste(missing_genes, collapse = ", "))) #this worked... trying to phase out in favor of warning message
+      #warning message
+      shinyalert::shinyalert(
+        title = "Warning: Invalid Input",
+        text = paste("The following genes were not found:", paste(missing_genes, collapse = ", ")),
+        type = "warning")
+    } else {
+      # Generate feature plot
+      feature_plot <- FeaturePlot(object = seurat_data, features = gene_names, reduction = "pca")
+      
+      #assign featureplot to global env
+      assign("feature_plot", "new", envir = .GlobalEnv) #this doesnt seem to work... try declaring reactive vals at top of server script
+      
+      #make sure featureplot is not null before rendering plot
+      if (is.null(feature_plot)) {
+        output$nogenesfound <- renderText("No genes found!")
+      } else {
+        output$featurePlotPCA <- renderPlot({ feature_plot })
+      }
+    }
+  })
+  
+  #sampleGene logic
+  output$sampleGenes <- renderText({
+    seurat_data <- seuratData()
+    if (is.null(seurat_data))
+      return(NULL)
+    # Access the RNA assay
+    rna_assay <- seurat_data@assays$RNA
+    
+    # Get the gene names
+    gene_names <- rownames(rna_assay@data)
+    
+    # Set the random seed for reproducibility (optional)
+    set.seed(123)
+    
+    # Select 10 random genes
+    random_genes <- sample(gene_names, 10)
+    
+    # Return the random genes as a character string
+    paste(random_genes, collapse = ", ")
+  })
+  
+  #sample metadata columns logic
+  # output$sampleCols <- renderText({
+  #   seurat_data <- seuratData()
+  #   if (is.null(seurat_data))
+  #     return(NULL)
+  #   # Access the cols
+  #   seurat_cols <- seurat_data@meta.data
+  #   
+  #   # Get the gene names
+  #   seurat_colnames <- names(seurat_cols)
+  #   
+  #   
+  #   # Return the random genes as a character string
+  #   paste(seurat_colnames, collapse = ", ")
+  # })
+  
+  
+  ###update dimplot/violinplot dropdown###
+  observeEvent(input$seuratFile, {
+    req(seuratData()) # wait for file input
+    seurat_data <- seuratData()
+
+    #fetch metadata colnames
+    obj_meta <- seurat_data@meta.data
+    
+    # Specify the metadata columns you want to check and convert
+    columns_to_check <- c("sample", "cytotoxic", "exact_subclonotype_id")
+    
+    # Find and convert columns that match the specified names
+    for (col in columns_to_check) {
+      if (col %in% colnames(obj_meta) && !is.factor(obj_meta[[col]])) {
+        obj_meta[[col]] <- as.factor(obj_meta[[col]])
+      }
+    }
+
+    #assign column factors to categorical_cols var
+    # categorical_cols <- names(obj_meta)[sapply(obj_meta, is.factor)]
+    categorical_cols <- names(obj_meta)[sapply(obj_meta, function(col) is.factor(col) && nlevels(col) < 5)]
+    
+
+    #reassign dropdown options (OLD)
+    updateSelectInput(session, "variableInput",label = "Select a Field to Split Dimplot By", choices=categorical_cols)
+
+    updateSelectInput(session, "violinInput",label = "Select a Field to Split Dimplot By", choices = categorical_cols)
+
+
+    
+    # Check and convert metadata columns that are not factors ####old way: ERRORS IF THE COLUMNS DONT EXIST#####
+    # for (col in columns_to_check) {
+    #   if (!is.factor( obj_meta[[col]])) {
+    #     obj_meta[[col]] <- as.factor( obj_meta[[col]])
+    #   }
+    # }
+    #categorical colnames only
+    # categorical_cols <- names(obj_meta)[sapply(obj_meta, is.factor)]
+    #render cat cols
+    # output$catcols <- renderText({categorical_cols})
+  })
+  
+  ###dimplot logic### 
+  # observeEvent(input$plotButton_Dim, {
+  #   req(seuratData()) # wait for file input
+  #   seurat_data <- seuratData()
+  #   if (is.null(seurat_data)) {
+  #     output$errormessage <- renderText("Null file")
+  #   } else {
+  #     # variable <- switch(input$variableInput,
+  #     #                    "Clusters" = "seurat_clusters",
+  #     #                    "Treatments" = "sample",
+  #     #                    "Clonotype" = "exact_subclonotype_id",
+  #     #                    "Cytotoxic" = "cytotoxic")
+  # 
+  # 
+  #     # Generate dim plot
+  #     dim_plot <- DimPlot(object = seurat_data, reduction = "umap", split.by=input$variableInput, group.by = input$variableInput) #input$variableInput (use this when autopopulate dropdown)
+  #     
+  #     # make sure dimplot is not null before rendering plot
+  #     if (is.null(dim_plot)) {
+  #       output$nodimplot <- renderText("Bad argument")
+  #     } else {
+  #       output$featurePlotDim <- renderPlot({ dim_plot })
+  #     }
+  #   }
+  # })
+  # 
+  ##### WIP #####
+  #new dimplot logic
+  observeEvent(input$plotButton_Dim, {
+    req(seuratData()) # wait for file input
+    seurat_data <- seuratData()
+    
+    if (is.null(seurat_data)) {
+      output$errormessage <- renderText("Null file")
+    } else {
+      if (!input$splitToggle) {
+        # Unsplit dim plot colored by group
+        dim_plot <- DimPlot(object = seurat_data, reduction = "umap", group.by = input$variableInput)
+        
+        if (is.null(dim_plot)) {
+          output$nodimplot <- renderText("Bad argument")
+        } else {
+          output$featurePlotDim <- renderPlot({ dim_plot })
+          output$splitToggle <- renderUI({ NULL }) # Hide the splitToggle checkbox
+        }
+      } else {
+        # Split dim plot colored by group
+        dim_plot <- DimPlot(object = seurat_data, reduction = "umap", split.by = input$variableInput, group.by = input$variableInput)
+        
+        if (is.null(dim_plot)) {
+          output$nodimplot <- renderText("Bad argument")
+        } else {
+          output$featurePlotDim <- renderPlot({ dim_plot })
+          output$splitToggle <- renderUI({ checkboxInput("splitToggle", "Split Dim Plot", value = TRUE) }) # Show the splitToggle checkbox
+        }
+      }
+    }
+  })
+  
+  ###
+  
+  #update violinplot dropdown#
+  
+  
+  ### violinplot logic ###
+  observeEvent(input$plotButton_violin, {
+    req(seuratData(),input$featuresInput_vln,input$violinInput) #wait for all inputs
+    seurat_data <- seuratData()
+   
+    # Split the input string @ commas into individual gene names using regex
+    gene_names <- strsplit(input$featuresInput_vln, ",\\s*")[[1]] 
+    gene_names <- trimws(gene_names)  # Trim leading and trailing spaces from gene names
+    
+    # Check if any of the requested genes are missing
+    missing_genes_vln <- setdiff(gene_names, rownames(seurat_data@assays$RNA@data))
+    
+    if (length(missing_genes_vln) > 0) {
+      #warning message
+      shinyalert::shinyalert(
+        title = "Warning: Invalid Input",
+        text = paste("The following genes were not found:", paste(missing_genes_vln, collapse = ", ")),
+        type = "warning")
+    } else {
+      # violinSelect <- switch(input$violinInput,
+      #                    "None" = NULL,
+      #                    "Clusters" = "seurat_clusters",
+      #                    "Treatments" = "sample")
+      
+      # Generate feature plot
+      violin_plot <- VlnPlot(object = seurat_data, features = gene_names, split.by =input$violinInput)
+      
+      #make sure featureplot is not null before rendering plot
+      if (is.null(violin_plot)) {
+        output$badviolinplot <- renderText("Bad argument")
+      } else {
+        output$violinplot <- renderPlot({ violin_plot })
+      }
+    }
+  })
+  
+  ##### WIP #####
+  
+  
+  
+  
+  ###Volcano plot logic### (7/5/23) incomplete... need sung feedback on what user inputs should be
+  # observeEvent(input$plotButton_Volc, {
+  #   req(seuratData()) # wait for file input
+  #   seurat_data <- seuratData()
+  #   if (is.null(seurat_data)) {
+  #     output$errormessage <- renderText("Null file")
+  #   } else {
+  #     # variable <- switch(input$variableInput,
+  #     #                    "Clusters" = "seurat_clusters",
+  #     #                    "Treatments" = "sample",
+  #     #                    "Clonotype" = "exact_subclonotype_id",
+  #     #                    "Cytotoxic" = "cytotoxic")
+  #     
+  #     # Generate volc plot
+  #     Volc_plot <- DimPlot(object = seurat_data, reduction = "umap", split.by = input$variableInput)
+  #     
+  #     # make sure volc is not null before rendering plot
+  #     if (is.null( Volc_plot)) {
+  #       output$noVolc <- renderText("Bad argument")
+  #     } else {
+  #       output$Volcplot <- renderPlot({  Volc_plot })
+  #     }
+  #   }
+  # })
+  
+  # #samplesplit logic
+  # output$samplesplit <- renderText({
+  #   seurat_data <- seuratData()
+  #   if (is.null(seurat_data))
+  #     return(NULL)
+  #   # Access the metadata
+  #   meta <- seurat_data@meta.data
+  #   
+  #   meta_cols <-names(meta)
+  #   
+  #   
+  #   # Return the params as a character string
+  #   paste(meta_cols, collapse = ", ")
+  # })
+    
+  
+  # troubleshooting ui x server comms
+  
+  # Render the input text
+  # output$inputz <- renderText({
+  #   # input$featuresInput
+  #   gene_names
+  # })
+  #test to see if webapp communicate ui and server
+  # output$outputText <- renderPrint({
+  #   input$textIn
+  # })
+  
+  ###
+}
+    
+  
+    
---
+++ myUI_mod.R
@@ -0,0 +1,205 @@
+#increase filesize limit
+options(shiny.maxRequestSize=10000*1024^2) #max 10gb upload
+
+#load libraries
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(purrr)
+library(stringr)
+library(Seurat)
+library(SeuratObject)
+library(DT)
+library(shinyalert)
+library(grDevices)
+library(ggplot2)
+
+# fully defined ui
+dashboardPage(
+  
+  # webapp layout #
+  dashboardHeader(title = "RDS Vis v1"),
+  menu_bar <- dashboardSidebar(
+                sidebarMenu(
+                  menuItem("Home", tabName = "home", icon = icon("home")),
+                  menuItem("Data Summary", tabName="input_summ", icon=icon("table")),
+                  menuItem("Feature Plot", tabName = "input", icon = icon("chart-bar")),
+                  menuItem("Dim Plot", tabName = "dim", icon = icon("cubes")),
+                  menuItem("Violin Plot", tabName = "violin", icon = icon("record-vinyl")),
+                  # menuItem("Volcano Plot", tabName = "volcano", icon = icon("fire")),
+                  menuItem("Help Page", tabName = "help", icon = icon("question"))
+                )
+              ),
+  dashboardBody(
+    tabItems(
+      tab_home <- tabItem(tabName = "home",
+                          h2("Home Page"),
+                          br(),
+                          br(),
+                          p("Welcome to the RDS Vis Tool!"),
+                          br(),
+                          p("This web app is designed for the visualization and exploration of single-cell RNA-seq data contained in Seurat objects. 
+                             It will provide various interactive plots and features to help you analyze and gain insights from your data. 
+                             Currently, this webapp does not support use of more than 1 GB of memory due to instance size restrictions of shinyapps.io's base plan. 
+                             As such, while the local version that we have been testing does not require the user to refresh between .rds file uploads, the server version you are using in some cases will do so.
+                             This webapp in its current state only exists to give users a sense of the page layout and to give developers insight into user requests.
+                             This memory restriction will no longer exist once the webapp is deployed on DNAnexus, which will lead to a more smooth and streamlined user experience."),
+                          br(),
+                          
+                          #embed href in paragraph sentence
+                          p("Click ", a("here", target="_blank", href="RDS_Vis_1.1.pdf", .noWS = "outside"), " for an in depth user guide.", .noWS = c("after-begin", "before-end")),
+                          
+                          # Add any additional content or UI elements here
+      ),
+      tab_input <- tabItem(tabName = "input_summ",
+                           h2("Data Summary Page"),
+                           br(),
+                           #file upload
+                           p("Click on the browse button and select the .rds file you wish to generate a feature plot for (Max 10 GB). 
+                                     Once your upload has completed, some sample genes from the Seurat object will populate below."),
+                           fileInput("seuratFile", 
+                                     "Upload RDS File",
+                                     accept = ".rds"),
+                           br(),
+                           p("Table of Seurat object metadata populates below:"),
+                           dataTableOutput("table")
+      ),
+      tab_inputfeatures <- tabItem(tabName = "input",
+                                   h2("Feature Plot Page"),
+                                   br(),
+                                  
+                                   br(),
+                                   p("Try some of these sample genes:"),
+                                   br(),
+                                   verbatimTextOutput("sampleGenes"),
+                                   br(),
+                                   p("Enter the feature of interest and then navigate between the UMAP and PCA subtabs to generate a feature plot for each respective reduction. Note that gene names are case sensitive."),
+                                   br(),
+                                   textInput("featuresInput", "Enter Gene Name:"),
+                                   br(),
+                                   #umap for multiple visualization tabs
+                                   tabsetPanel(
+                                     tabPanel("UMAP",
+                                              br(),
+                                              actionButton("plotButton_UMAP","Generate UMAP Plot"),
+                                              br(),
+                                              plotOutput("featurePlotUMAP", height = 600, width = 600)
+                                              
+                                     ),
+                                     tabPanel("PCA",
+                                              br(),
+                                              actionButton("plotButton_PCA","Generate PCA Plot"),
+                                              br(),
+                                              plotOutput("featurePlotPCA", height = 600, width = 600))
+                                     
+                                   )
+                                   
+                                   # 
+                                   # tabPanel("UMAP", plotOutput("featurePlot")),
+                                   # br(),
+                                   # textOutput("nofeaturefound"),
+                                   # br()
+                                   
+      ),
+      # tab_input,
+      # tab_feature,
+      tab_dim <- tabItem(tabName = "dim",
+                         h2("Dim Plot"),
+                         br(),
+                         p("This page generates DimPlots that can be split by different groups 
+                           found in a dropdown that updates based on the metadata columns in 
+                           the uploaded Seurat object. By default, the Dimplot will not be split by a group. 
+                           To split the visualization by a group, toggle the checkbox and select a group from the dropdown."),
+                         br(),
+                         checkboxInput("splitToggle", "Split Dim Plot", value = FALSE),
+                         
+                         br(),
+                         # selectInput("variableInput", label = NULL,
+                         #             choices = c("Clusters", "Treatments", "Clonotype", "Cytotoxic" )),
+                         selectInput("variableInput", label = "Select a Group:",
+                                     choices = "Input File For Dropdown Options"),
+                   
+                         br(),
+  
+                         br(),
+                         actionButton("plotButton_Dim","Generate DimPlot"),
+                         br(),
+                         plotOutput("featurePlotDim", height = 600, width = 900)
+
+      ),
+      tab_violin <- tabItem(tabName = "violin",
+                            h2("Violin Plot Page"),
+                            # Add violin plot page content here
+                            br(),
+                            p("Enter the feature of interest and then select a category to split the plot by:"),
+                            br(),
+                            textInput("featuresInput_vln", "Enter Gene Name:"),
+                            br(),
+                            selectInput("violinInput", label = "Select a Group:",
+                                        choices = "Input File For Dropdown Options"),
+                            br(),
+                            actionButton("plotButton_violin","Generate Violin Plot"),
+                            br(),
+                            plotOutput("violinplot", height = 600, width = 900)
+      ),
+      
+      tab_help <- tabItem(tabName = "help",
+                          h2("Help Page"),
+                          br(),
+                          p("Below are a list of potential FAQs (Subject to change)."),
+                          br(),
+                          accordion(
+                            id = "helpAccordion",
+                            accordionItem(
+                              title = "Why is my input file not loading?",
+                              "Check if the file path is correct and if the file format is compatible with the application. This webapp currently only supports .rds files with an upper size limitation of 10 GB."
+                            ),
+                            accordionItem(
+                              title = "How do I download these figures?",
+                              "To download the figures, you can right-click on each figure and choose the 'Save Image As' option. Then, select your desired location on your computer to save the image."
+                            ),
+                            accordionItem(
+                              title = "Is there any way to view tabular representations of the Seurat object?",
+                              "Currently, the server does have prebuilt logic to generate tables of the Seurat object's metadata. It is simply not present in the current version of the user interface to prevent visually bogging down the webapp and general ease of use. "
+                            )
+                          ),
+                          br(),
+                          br(),
+                          br(),
+                          #placeholder to make footer show on help page
+                          div(id = "placeholder",p("~ More FAQs in Progress! ~"))
+      )
+    ), 
+    div(class = "hr-container",
+        hr(class = "hr-line")),
+    # Footer
+    tags$footer(
+      class = "footer",
+      p("Developed for the Belfer Center Research Team by the BxMD Group at DFCI.")
+    ),
+    
+    
+    # CSS Styling
+    tags$style(HTML("
+      .hr-container {
+        text-align: center;
+      }
+      
+      .hr-line {
+        border-top: 1px solid #ccc;
+        margin: 20px 0;
+      }
+      
+      .footer {
+        padding: 10px;
+        text-align: center;
+      }
+      .placeholder {
+      text-align: center;
+      }
+      .full-width-cols {
+      overflow:auto;
+      }
+    "))
+  )
+)
