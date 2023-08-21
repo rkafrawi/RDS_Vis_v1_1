8e96ee1147bc55a316d8ff6bd18ce2b63f7c56c6
rkafrawi <98607591+rkafrawi@users.noreply.github.com>
2023-08-21 11:41:03 -0400

    Update myServer.R

--- myServer.R
+++ myServer.R
@@ -1,7 +1,10 @@
-# load vis source files:
+
+
+#########           SERVER START          ##########
+
 
 #define server
-function(input, output, session) {
+server<- function(input, output, session) {
   
   ### Define reactive values for global vars...next ver ###
   # seuratData <- reactiveVal()
@@ -16,7 +19,7 @@ function(input, output, session) {
     
     #read Seurat object from selected file
     seurat_obj <- readRDS(inFile$datapath)
-  
+    
     return(seurat_obj)
   })
   
@@ -97,7 +100,7 @@ function(input, output, session) {
       feature_plot <- FeaturePlot(object = seurat_data, features = gene_names, reduction = "pca")
       
       #assign featureplot to global env
-      assign("feature_plot", "new", envir = .GlobalEnv) #this doesnt seem to work... try declaring reactive vals at top of server script
+      # assign("feature_plot", "new", envir = .GlobalEnv) #this doesnt seem to work... try declaring reactive vals at top of server script
       
       #make sure featureplot is not null before rendering plot
       if (is.null(feature_plot)) {
@@ -129,28 +132,12 @@ function(input, output, session) {
     paste(random_genes, collapse = ", ")
   })
   
-  #sample metadata columns logic
-  # output$sampleCols <- renderText({
-  #   seurat_data <- seuratData()
-  #   if (is.null(seurat_data))
-  #     return(NULL)
-  #   # Access the cols
-  #   seurat_cols <- seurat_data@meta.data
-  #   
-  #   # Get the gene names
-  #   seurat_colnames <- names(seurat_cols)
-  #   
-  #   
-  #   # Return the random genes as a character string
-  #   paste(seurat_colnames, collapse = ", ")
-  # })
-  
   
   ###update dimplot/violinplot dropdown###
   observeEvent(input$seuratFile, {
     req(seuratData()) # wait for file input
     seurat_data <- seuratData()
-
+    
     #fetch metadata colnames
     obj_meta <- seurat_data@meta.data
     
@@ -163,58 +150,18 @@ function(input, output, session) {
         obj_meta[[col]] <- as.factor(obj_meta[[col]])
       }
     }
-
+    
     #assign column factors to categorical_cols var
     # categorical_cols <- names(obj_meta)[sapply(obj_meta, is.factor)]
     categorical_cols <- names(obj_meta)[sapply(obj_meta, function(col) is.factor(col) && nlevels(col) < 5)]
     
-
-    #reassign dropdown options (OLD)
+    #reassign dropdown options
     updateSelectInput(session, "variableInput",label = "Select a Field to Split Dimplot By", choices=categorical_cols)
-
-    updateSelectInput(session, "violinInput",label = "Select a Field to Split Dimplot By", choices = categorical_cols)
-
-
     
-    # Check and convert metadata columns that are not factors ####old way: ERRORS IF THE COLUMNS DONT EXIST#####
-    # for (col in columns_to_check) {
-    #   if (!is.factor( obj_meta[[col]])) {
-    #     obj_meta[[col]] <- as.factor( obj_meta[[col]])
-    #   }
-    # }
-    #categorical colnames only
-    # categorical_cols <- names(obj_meta)[sapply(obj_meta, is.factor)]
-    #render cat cols
-    # output$catcols <- renderText({categorical_cols})
+    updateSelectInput(session, "violinInput",label = "Select a Field to Split Dimplot By", choices = categorical_cols)
+    
   })
   
-  ###dimplot logic### 
-  # observeEvent(input$plotButton_Dim, {
-  #   req(seuratData()) # wait for file input
-  #   seurat_data <- seuratData()
-  #   if (is.null(seurat_data)) {
-  #     output$errormessage <- renderText("Null file")
-  #   } else {
-  #     # variable <- switch(input$variableInput,
-  #     #                    "Clusters" = "seurat_clusters",
-  #     #                    "Treatments" = "sample",
-  #     #                    "Clonotype" = "exact_subclonotype_id",
-  #     #                    "Cytotoxic" = "cytotoxic")
-  # 
-  # 
-  #     # Generate dim plot
-  #     dim_plot <- DimPlot(object = seurat_data, reduction = "umap", split.by=input$variableInput, group.by = input$variableInput) #input$variableInput (use this when autopopulate dropdown)
-  #     
-  #     # make sure dimplot is not null before rendering plot
-  #     if (is.null(dim_plot)) {
-  #       output$nodimplot <- renderText("Bad argument")
-  #     } else {
-  #       output$featurePlotDim <- renderPlot({ dim_plot })
-  #     }
-  #   }
-  # })
-  # 
-  ##### WIP #####
   #new dimplot logic
   observeEvent(input$plotButton_Dim, {
     req(seuratData()) # wait for file input
@@ -256,7 +203,7 @@ function(input, output, session) {
   observeEvent(input$plotButton_violin, {
     req(seuratData(),input$featuresInput_vln,input$violinInput) #wait for all inputs
     seurat_data <- seuratData()
-   
+    
     # Split the input string @ commas into individual gene names using regex
     gene_names <- strsplit(input$featuresInput_vln, ",\\s*")[[1]] 
     gene_names <- trimws(gene_names)  # Trim leading and trailing spaces from gene names
@@ -271,11 +218,6 @@ function(input, output, session) {
         text = paste("The following genes were not found:", paste(missing_genes_vln, collapse = ", ")),
         type = "warning")
     } else {
-      # violinSelect <- switch(input$violinInput,
-      #                    "None" = NULL,
-      #                    "Clusters" = "seurat_clusters",
-      #                    "Treatments" = "sample")
-      
       # Generate feature plot
       violin_plot <- VlnPlot(object = seurat_data, features = gene_names, split.by =input$violinInput)
       
@@ -287,67 +229,7 @@ function(input, output, session) {
       }
     }
   })
-  
-  ##### WIP #####
-  
-  
-  
-  
-  ###Volcano plot logic### (7/5/23) incomplete... need sung feedback on what user inputs should be
-  # observeEvent(input$plotButton_Volc, {
-  #   req(seuratData()) # wait for file input
-  #   seurat_data <- seuratData()
-  #   if (is.null(seurat_data)) {
-  #     output$errormessage <- renderText("Null file")
-  #   } else {
-  #     # variable <- switch(input$variableInput,
-  #     #                    "Clusters" = "seurat_clusters",
-  #     #                    "Treatments" = "sample",
-  #     #                    "Clonotype" = "exact_subclonotype_id",
-  #     #                    "Cytotoxic" = "cytotoxic")
-  #     
-  #     # Generate volc plot
-  #     Volc_plot <- DimPlot(object = seurat_data, reduction = "umap", split.by = input$variableInput)
-  #     
-  #     # make sure volc is not null before rendering plot
-  #     if (is.null( Volc_plot)) {
-  #       output$noVolc <- renderText("Bad argument")
-  #     } else {
-  #       output$Volcplot <- renderPlot({  Volc_plot })
-  #     }
-  #   }
-  # })
-  
-  # #samplesplit logic
-  # output$samplesplit <- renderText({
-  #   seurat_data <- seuratData()
-  #   if (is.null(seurat_data))
-  #     return(NULL)
-  #   # Access the metadata
-  #   meta <- seurat_data@meta.data
-  #   
-  #   meta_cols <-names(meta)
-  #   
-  #   
-  #   # Return the params as a character string
-  #   paste(meta_cols, collapse = ", ")
-  # })
-    
-  
-  # troubleshooting ui x server comms
-  
-  # Render the input text
-  # output$inputz <- renderText({
-  #   # input$featuresInput
-  #   gene_names
-  # })
-  #test to see if webapp communicate ui and server
-  # output$outputText <- renderPrint({
-  #   input$textIn
-  # })
-  
-  ###
-}
-    
-  
-    
+} #end of server
+
+#########           SERVER END          ##########
+
