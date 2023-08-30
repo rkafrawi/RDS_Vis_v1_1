db56253c32f499ae2e7e8303688a8ee976e755b1
rkafrawi <98607591+rkafrawi@users.noreply.github.com>
2023-08-30 15:46:57 -0400

    Add files via upload

---
+++ DNAnexus_deploy/rds_visualizer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# flying_kmeans 0.0.1
+set -eux
+main() {
+ # get K-means app code
+ mkdir rds_vizualizer
+ url=https://raw.githubusercontent.com/rkafrawi/RDS_Vis_v1_1/main/Webapp_source
+ wget -P rds_visualizer/ $url/DESCRIPTION $url/server.R $url/ui.R
+ # pull and run Shiny Server docker image
+ dx download $DX_PROJECT_CONTEXT_ID:/rk_shiny/rds_vis_maria.tar.gz
+ docker load -i rds_vis_maria.tar.gz
+ # attach our K-means app's folder as a volume
+ docker run --rm -p 443:3838 -v $PWD/rds_visualizer:/srv/shiny-server/ rds_vis_maria
+}
