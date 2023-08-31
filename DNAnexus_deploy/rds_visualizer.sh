2b086971288b7c853cbd1c67e2bc776a7e5101d3
rkafrawi <98607591+rkafrawi@users.noreply.github.com>
2023-08-31 15:53:04 -0400

    Update rds_visualizer.sh

--- DNAnexus_deploy/rds_visualizer.sh
+++ DNAnexus_deploy/rds_visualizer.sh
@@ -6,9 +6,11 @@ main() {
  mkdir rds_vizualizer
  url=https://raw.githubusercontent.com/rkafrawi/RDS_Vis_v1_1/main/Webapp_source
  wget -P rds_visualizer/ $url/DESCRIPTION $url/server.R $url/ui.R
+ 
  # pull and run Shiny Server docker image
  dx download $DX_PROJECT_CONTEXT_ID:/rk_shiny/rds_vis_maria.tar.gz
  docker load -i rds_vis_maria.tar.gz
+ 
  # attach our rds_vis app's folder as a volume
  docker run --rm -p 443:3838 -v $PWD/rds_visualizer:/srv/shiny-server/ rds_vis_maria
 }
